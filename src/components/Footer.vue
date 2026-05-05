f1b76a726974cc18e3bd447be54de7ca40e5792f
AlexanderM1927 <jhon71937@gmail.com>
2026-05-05 16:53:42 -0500

    Add ig and github icons at footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,27 @@
   <div class="footer">
     <div class="footer__content">
       <div class="footer__brand">
-        <p class="footer__logo">Alexander<span>M</span></p>
+        <p class="footer__logo">
+          Alexander<span>M</span>
+          <a
+            href="https://instagram.com/alexanderm.co"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="footer__instagram"
+            aria-label="Instagram"
+          >
+            <q-icon name="fa-brands fa-instagram" size="1.2em" />
+          </a>
+          <a
+            href="https://github.com/alexanderm1927"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="footer__github"
+            aria-label="GitHub"
+          >
+            <q-icon name="fa-brands fa-github" size="1.2em" />
+          </a>
+        </p>
         <p class="footer__tagline">{{ $t('footer.tagline') }}</p>
       </div>
       <div class="footer__copy">
